d74ff01ab3bf13caf4e0074eae9b4b4f3bf8448a
Maurizio Napolitano <napo@fbk.eu>
2026-09-24 22:04:55 +0200

    github page

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -1,17 +1,19 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  base: '/analisirefertogara/',
+
   server: {
     host: '0.0.0.0',
     port: 3000,
     allowedHosts: true,
   },
+
   preview: {
     host: '0.0.0.0',
     port: 3000,
   },
 })
-
